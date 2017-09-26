cb339fda5f011079589f1ee4edf6c1d049b863d8
Jason Fellows <jfellows@student.cvtc.edu>
2017-09-26 19:30:46 -0400

    intial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def index
+    @number_of_recipes = Recipe.count
+    @latest_recipe = Recipe.last
+  end
+
+  def about
+  end
+  
+  def contact
+  end
+end
